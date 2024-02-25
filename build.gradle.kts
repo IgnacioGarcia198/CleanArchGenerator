c1a9850ec6fcb5c3f71029d1fd306422061213bc
IgnacioGarcia198 <ignaciogarcia198@gmail.com>
2024-02-25 16:22:57 +0100

    Update version and group in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.13.3"
 }
 
-group = "com.example"
-version = "1.0-SNAPSHOT"
+group = "com.garcia.ignacio.cleanarchgenerator"
+version = "0.1"
 
 repositories {
     mavenCentral()
